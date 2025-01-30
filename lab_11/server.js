445b40c5b1a801094f6798c7531228b708d5fc1a
RonanCraib <ronancraib@outlook.com>
2025-01-30 10:55:22 +0000

    11.6

--- lab_11/server.js
+++ lab_11/server.js
@@ -11,5 +11,16 @@ app.get('/add', function(req, res){
     var y = parseInt(req.query.y); 
     res.send("X + Y = " + (x + y)); 
     });
+app.get('/calc', function(req, res){ 
+    var x = parseFloat(req.query.x); 
+    var y = parseFloat(req.query.y); 
+    var operator = req.query.operator;
+        
+    if (operator === "add") res.send("Result = " + (x + y));
+    else if (operator === "sub") res.send("Result = " + (x - y));
+    else if (operator === "mul") res.send("Result = " + (x * y));
+    else if (operator === "div") res.send("Result = " + (x / y));
+    else res.send("Invalid operator");
+    });
     
 app.listen(8080);
