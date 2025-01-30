561b8b43b43548835e92d34aa729878f6a8d713e
RonanCraib <ronancraib@outlook.com>
2025-01-30 11:13:27 +0000

    11.9

--- lab_11/server.js
+++ lab_11/server.js
@@ -27,6 +27,13 @@ app.get('/calc', function(req, res){
     else if (operator === "div") res.send("Result = " + (x / y));
     else res.send("Invalid operator");
     });
+
+app.get('/getform', function(req, res){ 
+    var name = req.query.name; 
+    var quest = req.query.quest; 
+        res.send("Hi "+name+" I am sure you will "+quest) ; 
+    });
+        
     
 
 app.listen(8080);
