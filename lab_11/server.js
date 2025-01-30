eec534d190ef498ee3e93d9a8b6580f4be28e72e
RonanCraib <ronancraib@outlook.com>
2025-01-30 10:49:27 +0000

    11.5

--- lab_11/server.js
+++ lab_11/server.js
@@ -7,8 +7,9 @@ app.get('/test', function(req, res){
     res.send("this is route 2"); 
     });
 app.get('/add', function(req, res){ 
-    var x = req.query.x; 
-    var y = req.query.y; 
-        res.send("X + Y="+(x+y)); 
+    var x = parseInt(req.query.x); 
+    var y = parseInt(req.query.y); 
+    res.send("X + Y = " + (x + y)); 
     });
+    
 app.listen(8080);
