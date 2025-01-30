eae754d671d1606e69d7ac2064659110863f4257
RonanCraib <ronancraib@outlook.com>
2025-01-30 10:41:38 +0000

    11.3

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,6 +1,6 @@
 var express = require('express'); 
 var app = express(); 
 app.get('/', function(req, res){ 
-   res.send("Hello world! by express”); 
-}); 
-app.listen(8080); 
+   res.send("Hello world! by express"); 
+    }); 
+app.listen(8080);
